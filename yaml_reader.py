cb0bf488e4f8db3a37fc06b65d76cec8ef997b85
Param <parambytes@gmail.com>
2024-05-13 12:27:22 -0400

    yaml correction

--- yaml_reader.py
+++ yaml_reader.py
@@ -7,6 +7,7 @@ class YamlPipelineExecutor():
     def __init__(self, pipeline_location):
         self._pipeline_location = pipeline_location
         self._queues = {}
+        self._workers = {}
 
     def _load_pipeline(self):
         with open(self._pipeline_location, 'r') as file:
@@ -18,24 +19,24 @@ class YamlPipelineExecutor():
             queue_name = queue['name']
             self._queues[queue_name] = Queue()
 
-
     def _initialize_workers(self):
         for worker in self._yaml_data['workers']:
-            WorkerClass = getattr(importlib.import_module(worker['location']), worker['class'])
+            Worker_class = getattr(importlib.import_module(worker['location'])  , worker['class'])
             input_queue = worker.get('input_queue')
-            output_queues = worker.get('output_queue')
+            output_queues = worker.get('output_queues')
+            num_instances = worker.get("instances", 1)
             worker_name = worker['name']
-            num_instances = worker.get('instances', 1)
 
-            init__params = {
-                'input_queue': self.queue[input_queue] if input_queue is not None else None,
-                'output_queue': [self._queue[output_queue] for output_queue in output_queues]\
-                      if output_queues is not None else None
+
+            int_params = {
+                'input_queue': self._queues[input_queue] if input_queue is not None else None,
+                'output_queues': [self._queues[output_queues] for queue in output_queues] \
+                    if output_queues is not None else None
             }
 
-            self._workers[worker_name] = []
-            for i in range(num_instances):
-                self.worker[worker_name].append(WorkerClass(**init__params))
+        self._workers[worker_name] = []
+        for i in range(num_instances):
+            self._workers[worker_name.append(Worker_class(**int_params))]
 
 
     def __join_workers(self):
